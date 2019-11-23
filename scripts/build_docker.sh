d50f8a6b3da6b6be6acdb95f4287aa1cb1d4be0d
Antonin RAFFIN <antonin.raffin@ensta.org>
2019-11-23 23:55:33 +0100

    Update docker image version

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -4,7 +4,7 @@ CPU_PARENT=ubuntu:16.04
 GPU_PARENT=nvidia/cuda:9.0-cudnn7-runtime-ubuntu16.04
 
 TAG=stablebaselines/stable-baselines
-VERSION=v2.9.0
+VERSION=v3.0.0
 
 if [[ ${USE_GPU} == "True" ]]; then
   PARENT=${GPU_PARENT}
@@ -14,4 +14,3 @@ else
 fi
 
 docker build --build-arg PARENT_IMAGE=${PARENT} -t ${TAG}:${VERSION} .
-
