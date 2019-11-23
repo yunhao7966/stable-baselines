446fca4140fb692a0f00e9953c7c0c2d3cfad8ea
Antonin RAFFIN <antonin.raffin@ensta.org>
2019-11-23 23:39:10 +0100

    Fix Dockerfile build for gpu

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 ARG PARENT_IMAGE
-ARG USE_GPU
+ARG TENSORFLOW_PACKAGE
 FROM $PARENT_IMAGE
 
 RUN apt-get -y update \
@@ -33,11 +33,6 @@ RUN \
     . $VENV/bin/activate && \
     cd $CODE_DIR && \
     pip install --upgrade pip && \
-    if [[ $USE_GPU == "True" ]]; then \
-        TENSORFLOW_PACKAGE="tensorflow-gpu"; \
-    else \
-        TENSORFLOW_PACKAGE="tensorflow"; \
-    fi; \
     pip install ${TENSORFLOW_PACKAGE} && \
     pip install -e .[mpi,tests] && \
     pip install codacy-coverage && \
--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -4,14 +4,16 @@ CPU_PARENT=ubuntu:16.04
 GPU_PARENT=nvidia/cuda:9.0-cudnn7-runtime-ubuntu16.04
 
 TAG=stablebaselines/stable-baselines
-VERSION=v2.9.0
+VERSION=v3.0.0
 
 if [[ ${USE_GPU} == "True" ]]; then
   PARENT=${GPU_PARENT}
+  TENSORFLOW_PACKAGE="tensorflow-gpu"
 else
   PARENT=${CPU_PARENT}
+  TENSORFLOW_PACKAGE="tensorflow"
   TAG="${TAG}-cpu"
 fi
 
-docker build --build-arg PARENT_IMAGE=${PARENT} -t ${TAG}:${VERSION} .
-
+docker build --build-arg PARENT_IMAGE=${PARENT} --build-arg TENSORFLOW_PACKAGE=${TENSORFLOW_PACKAGE}\
+ -t ${TAG}:${VERSION} .
